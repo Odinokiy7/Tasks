45d04a9a3474d16f2d6dcbfc5a865a4ea813dbf0
Odinokiy7 <koazo-maga@mail.ru>
2022-02-01 13:57:51 +0300

    task

---
+++ task/Program.cs
@@ -0,0 +1,5 @@
+﻿// Задача: Вывести квадрат числа
+Console.Write("Введите число: ");
+string numberA = Console.ReadLine();
+int number = int.Parse(numberA);
+Console.WriteLine("Квадрат числа: " + number * number);
