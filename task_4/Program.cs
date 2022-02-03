3a93b7b0647ce0ca53ce607dc05d155b7937748c
Odinokiy7 <koazo-maga@mail.ru>
2022-02-03 12:13:13 +0300

    task 4

---
+++ task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти максимальное из трех чисел.
+
+int a = 54;
+int b = 89;
+int c = 73;
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("Максимальное число: " + max);
