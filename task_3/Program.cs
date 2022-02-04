e7dadb889ffc3b29aaadc1543ee07c3526082707
Odinokiy7 <koazo-maga@mail.ru>
2022-02-04 15:47:29 +0300

    Task 3

---
+++ task_3/Program.cs
@@ -0,0 +1,34 @@
+﻿// По заданному номеру дня недели вывести его название
+
+Console.Write("Введите номер дня недели: ");
+string weekdays = Console.ReadLine();
+int weekday = int.Parse(weekdays);
+
+if (weekday == 1)
+{
+    Console.WriteLine("Понедельник");
+}
+if (weekday == 2)
+{
+    Console.WriteLine("Вторник");
+}
+if (weekday == 3)
+{
+    Console.WriteLine("Среда");
+}
+if (weekday == 4)
+{
+    Console.WriteLine("Четверг");
+}
+if (weekday == 5)
+{
+    Console.WriteLine("Пятница");
+}
+if (weekday == 6)
+{
+    Console.WriteLine("Суббота");
+}
+if (weekday == 7)
+{
+    Console.WriteLine("Воскресенье");
+}
