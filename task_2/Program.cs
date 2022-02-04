7b50d0e5b405589526b8f511629334165149040d
Odinokiy7 <koazo-maga@mail.ru>
2022-02-04 15:57:34 +0300

    Task 2

---
+++ task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Даны два числа. Показать большее и меньшее число.
+
+int number1 = 35;
+int number2 = 53;
+ if (number1 > number2)
+ {
+     Console.WriteLine("Большее число: " + number1);
+     Console.WriteLine("Меньшее число: " + number2);
+ }
+ else
+ {
+     Console.WriteLine("Меньшее число: " + number1);
+     Console.WriteLine("Большее число: " + number2);
+ }
