e8a2c709373ac24805049fd727f548e4f09fc6f1
Odinokiy7 <koazo-maga@mail.ru>
2022-02-04 09:32:53 +0300

    Task 13

---
+++ task_13/Program.cs
@@ -0,0 +1,35 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток   
+int GetNumber()
+
+{
+    int rundomNumber = new Random().Next(20, 100);
+    return rundomNumber;
+}
+
+void IsMultiple(int firstNumber, int secondNumber)
+{
+    if (firstNumber > secondNumber) 
+    {
+        int result = firstNumber % secondNumber;
+        if (result == 0)
+            Console.WriteLine("Первое число кратно второму ");
+        else
+        Console.WriteLine($"Остаток: {result} ");
+    }
+    else
+    {
+        int result = secondNumber % firstNumber;
+        if (result == 0)
+            Console.WriteLine("Второе число кратно первому ");
+        else
+        Console.WriteLine($"Остаток: {result}");
+    }
+}
+
+int number = GetNumber();
+Console.WriteLine($"Первое случайное число: {number}");
+
+int secondNumber = GetNumber();
+Console.WriteLine($"Второе случайное число: {secondNumber}");
+IsMultiple(number, secondNumber);
+
