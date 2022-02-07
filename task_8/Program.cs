a78ed1e816c21b8b68289ed6e661e5999595691c
Odinokiy7 <koazo-maga@mail.ru>
2022-02-07 16:11:20 +0300

    Task 8

---
+++ task_8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать четные числа от 1 до N
+
+int random = new Random().Next(2, 20);
+Console.Write($"Чётные числа от 1 до {random}:");
+
+int min = 2;
+while (min < random)
+{
+    if (min % 2 == 0)
+        Console.Write($" {min},");
+    min++;
+}
