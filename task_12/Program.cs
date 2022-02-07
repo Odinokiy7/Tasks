43f1bdf564bca7e7121cec9069623f122b657a8a
Odinokiy7 <koazo-maga@mail.ru>
2022-02-07 16:55:53 +0300

    Task 12

---
+++ task_12/Program.cs
@@ -0,0 +1,8 @@
+﻿// Удалить вторую цифру трехзначного числа
+
+int random = new Random().Next(100, 999);
+Console.WriteLine($"Трехзначное число: {random}");
+
+int myNumber = random / 100;
+int myNumber2 = random % 10;
+Console.WriteLine($"Вторая цифра удалена: {myNumber} {myNumber2}");
