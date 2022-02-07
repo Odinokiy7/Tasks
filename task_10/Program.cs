f8033c3007b0ca9c143228d740c04ae6a604601f
Odinokiy7 <koazo-maga@mail.ru>
2022-02-07 16:35:29 +0300

    Task 10

---
+++ task_10/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать вторую цифру трехзначного числа.
+
+int random = new Random().Next(100, 999);
+Console.WriteLine($"Трехзначное число: {random}");
+
+int myNumber = random % 100 / 10;
+Console.WriteLine($"Вторая цифра = {myNumber}");
