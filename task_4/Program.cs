bd535b07b3139dc4c0d0fd32c28919d8eb345d5b
Odinokiy7 <koazo-maga@mail.ru>
2022-02-07 12:36:08 +0300

    Task 4.2

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -3,10 +3,9 @@
 int a = 54;
 int b = 89;
 int c = 73;
+Console.WriteLine($"Даны три числа: {a}, {b}, {c}");
 
 int max = a;
-
 if (b > max) max = b;
 if (c > max) max = c;
-
 Console.WriteLine("Максимальное число: " + max);
