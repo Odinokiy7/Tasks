4f065f0ee3a5537a968fcfb76f2f8965f75b2656
Odinokiy7 <koazo-maga@mail.ru>
2022-02-08 16:25:53 +0300

    Task 16

---
+++ task_16/Program.cs
@@ -0,0 +1,9 @@
+﻿// 16. Дано число, обозначающее день недели. Выяснить, является ли номер дня недели выходным
+
+int random = new Random().Next(1, 8);
+Console.Write($"{random} день недели - ");
+
+if (random < 6)
+    Console.WriteLine("будний");
+else
+    Console.WriteLine("выходной");
