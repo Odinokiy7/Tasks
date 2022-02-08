f14d8fbd407b9832e4f89d84647a71055121ffc2
Odinokiy7 <koazo-maga@mail.ru>
2022-02-08 11:17:27 +0300

    Task 14

---
+++ task_14/Program.cs
@@ -0,0 +1,8 @@
+﻿// 14. Найти третью цифру числа или сообщить, что её нет
+
+int random = new Random().Next(0, 1000);
+Console.WriteLine($"Дано число: {random}");
+if (random > 99)
+    Console.WriteLine($"Третья цифра = {random % 10}");
+else
+    Console.WriteLine("Третьей цифры нет");
