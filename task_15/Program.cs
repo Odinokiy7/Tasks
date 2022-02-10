275f7036d66beba524a370291f97754b5085a51d
Odinokiy7 <koazo-maga@mail.ru>
2022-02-10 12:12:41 +0300

    Task 15

---
+++ task_15/Program.cs
@@ -0,0 +1,8 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+
+int number = 322;
+
+if (number % (7 * 23) == 0)
+Console.WriteLine($"{number} кратно 7 и 23");
+else
+Console.WriteLine($"{number} не кратно 7 и 23");
