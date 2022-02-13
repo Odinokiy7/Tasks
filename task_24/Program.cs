a2041e0c60fe09a3bf5fcfca653d663d1d17adc8
Odinokiy7 <koazo-maga@mail.ru>
2022-02-13 18:24:22 +0300

    Task 24.2

--- task_24/Program.cs
+++ task_24/Program.cs
@@ -1,10 +1,10 @@
 ﻿// 24. Найти кубы чисел от 1 до N
 
-Console.Write("Дан массив из чисел: ");
+Console.Write("Дан массив из чисел:");
 int[] massiv = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 int count = massiv.Length;
 for (int i = 0; i < count; i++)
-    Console.Write(massiv[i]);
+    Console.Write($" {massiv[i]}");
 Console.WriteLine();
 for (int i = 1; i <= massiv.Length; i++)
     Console.WriteLine($"Куб числа {i}: {i} * {i} * {i} = {i * i * i}");
