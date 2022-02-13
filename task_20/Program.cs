f3e17223518c5fc813d377197600d67b6c7c7985
Odinokiy7 <koazo-maga@mail.ru>
2022-02-13 17:27:58 +0300

    Task 20.2

--- task_20/Program.cs
+++ task_20/Program.cs
@@ -1,16 +1,20 @@
 ﻿// 20. Задать номер четверти, показать диапазоны для возможных координат
 
-Console.WriteLine("Введите номер четверти от 1 до 4: ");
+Console.Write("Введите номер четверти: ");
 string? numberStr = Console.ReadLine();
 int numberQuarter = int.Parse(numberStr);
 
-if (numberQuarter == 1)
-    Console.WriteLine("Диапазоны: x > 0, y > 0");
-else if (numberQuarter == 2)
-    Console.WriteLine("Диапазоны: x < 0, y > 0");
-else if (numberQuarter == 3)
-    Console.WriteLine("Диапазоны: x < 0, y < 0");
-else if (numberQuarter == 4)
-    Console.WriteLine("Диапазоны: x > 0, y < 0");
-else
-    Console.WriteLine("Данная четверть не найдена");
+void Method(int numberQuarter)
+{
+    if (numberQuarter == 1)
+        Console.WriteLine("Диапазоны: x > 0, y > 0");
+    else if (numberQuarter == 2)
+        Console.WriteLine("Диапазоны: x < 0, y > 0");
+    else if (numberQuarter == 3)
+        Console.WriteLine("Диапазоны: x < 0, y < 0");
+    else if (numberQuarter == 4)
+        Console.WriteLine("Диапазоны: x > 0, y < 0");
+    else
+        Console.WriteLine("Данная четверть не найдена");
+}
+Method(numberQuarter);
