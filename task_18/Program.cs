d3f11cfd3f962c54872878886029356720a93e3b
Odinokiy7 <koazo-maga@mail.ru>
2022-02-14 11:21:40 +0300

    Task 18

---
+++ task_18/Program.cs
@@ -0,0 +1,29 @@
+﻿// 18. Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+bool x = true;
+bool y = true;
+if (!(x || y) == (!x) && (!y))
+    Console.WriteLine(true);
+else
+    Console.WriteLine(false);
+
+bool x2 = false;
+bool y2 = false;
+if (!(x2 || y2) == (!x2) && (!y2))
+    Console.WriteLine(true);
+else
+    Console.WriteLine(false);
+
+bool x3 = true;
+bool y3 = false;
+if (!(x3 || y3) == (!x3) && (!y3))
+    Console.WriteLine(true);
+else
+    Console.WriteLine(false);
+
+bool x4 = false;
+bool y4 = true;
+if (!(x4 || y4) == (!x4) && (!y4))
+    Console.WriteLine(true);
+else
+    Console.WriteLine(false);
