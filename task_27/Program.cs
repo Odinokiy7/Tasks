0274717e60b77e4762724be316049e011755c55e
Odinokiy7 <koazo-maga@mail.ru>
2022-02-15 12:04:17 +0300

    Task 27

---
+++ task_27/Program.cs
@@ -0,0 +1,12 @@
+﻿// 27. Определить количество цифр в числе
+
+int number = new Random().Next(10, 100000);
+Console.WriteLine($"Дано число: {number}");
+
+int count = 0;
+while (number != 0)
+{
+    number = number / 10;
+    count++;
+}
+Console.WriteLine($"Количество цифр в числе = {count}");
