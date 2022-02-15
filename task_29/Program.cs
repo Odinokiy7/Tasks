aa49e3d9940ead1509af46775eac6b5eb1357afc
Odinokiy7 <koazo-maga@mail.ru>
2022-02-15 12:22:50 +0300

    Task 29.2

---
+++ task_29/Program.cs
@@ -0,0 +1,25 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+
+int insertNumber(string message)
+{
+    Console.Write(message);
+    string? numberStr = Console.ReadLine();
+    int number = int.Parse(numberStr);
+    return number;
+}
+
+int number = insertNumber("Введите число от 1 до N: ");
+int sum = 1;
+
+if (number == 0)
+{
+    Console.Write($"Введите число больше 0");
+}
+else
+{
+    for (int i = 1; i <= number; i++)
+    {
+        sum *= i; // Равнозначно sum = sum * i;
+    }
+    Console.WriteLine($"Произведение чисел от 1 до {number} = {sum}");
+}
