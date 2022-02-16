61f6a15235e8819a9dd03503837991555af78029
Odinokiy7 <koazo-maga@mail.ru>
2022-02-16 23:14:54 +0300

    Task 28.2

--- task_28/Program.cs
+++ task_28/Program.cs
@@ -1,9 +1,11 @@
 ﻿// 28. Подсчитать сумму цифр в числе
 
+Console.Clear();
+
 Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
 
-int sum = 0;
+double sum = 0;
 while (number > 0)
 {
     sum = sum + number % 10;
