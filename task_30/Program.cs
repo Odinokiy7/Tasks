c6a74eea062d5d5d54f0e3a5faa004e390a52869
Odinokiy7 <koazo-maga@mail.ru>
2022-02-16 19:56:22 +0300

    Task 30

---
+++ task_30/Program.cs
@@ -0,0 +1,14 @@
+﻿// 30. Показать кубы чисел, заканчивающихся на четную цифру
+
+Console.Write("Дан массив из чисел:");
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int count = array.Length;
+for (int i = 0; i < count; i++)
+    Console.Write($" {array[i]}");
+Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+        Console.WriteLine($"Куб числа {array[i]}: {array[i]} * {array[i]} * {array[i]} = {array[i] * array[i] * array[i]}");
+}
