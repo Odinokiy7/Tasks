df3d6eb8c3770f0d067b5014d5b6cff7d2af0613
Odinokiy7 <koazo-maga@mail.ru>
2022-02-16 23:16:58 +0300

    Task 26.2

--- task_26/Program.cs
+++ task_26/Program.cs
@@ -1,10 +1,17 @@
 ﻿// 26. Возведите число А в натуральную степень B используя цикл
 
+Console.Clear();
+
 Console.WriteLine("Возводим число A в натуральную степень B");
 Console.Write("a = ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("b = ");
 int b = Convert.ToInt32(Console.ReadLine());
 
-double result = Math.Pow(a, b);
-Console.WriteLine($"{a} в степени {b} = {result}");
+if (b > 0)
+{
+    double result = Math.Pow(a, b);
+    Console.WriteLine($"{a} в степени {b} = {result}");
+}
+else
+    Console.WriteLine("Ошибка!");
