a4dd9691642fbf6deb5c0d8e0b6742f0fb126fcf
Odinokiy7 <koazo-maga@mail.ru>
2022-02-16 09:33:26 +0300

    Task 31

---
+++ task_31/Program.cs
@@ -0,0 +1,9 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран
+
+Console.Write("Дан массив из чисел: ");
+int[] massiv = { 1, 2, 3, 4, 5, 6, 7, 8, };
+int count = massiv.Length;
+for (int i = 0; i < count; i++)
+{
+    Console.Write($" {massiv[i]}");
+}
