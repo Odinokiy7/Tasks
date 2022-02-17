ee4341fccde6a9457173733302faf0b7305316d7
Odinokiy7 <koazo-maga@mail.ru>
2022-02-17 14:08:55 +0300

    Task 9

---
+++ task_9/Program.cs
@@ -0,0 +1,9 @@
+﻿// 9. Показать последнюю цифру трёхзначного числа
+
+Console.Clear();
+
+int number = new Random().Next(100, 1000);
+Console.Write($"Дано число: {number}. ");
+
+number = number % 10;
+Console.WriteLine($"Последняя цифра: {number}");
