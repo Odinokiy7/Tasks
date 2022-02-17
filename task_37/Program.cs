4ec9b0ada683d5bd7e9c1f7ef1423321da94c0f1
Odinokiy7 <koazo-maga@mail.ru>
2022-02-17 15:48:50 +0300

    Task 37

---
+++ task_37/Program.cs
@@ -0,0 +1,31 @@
+﻿// 37. В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+Console.Clear();
+
+int[] massiv = new int[123];
+
+void FillArray()
+{
+    Random number = new Random();
+    Console.Write($"Дан массив из чисел: ");
+    for (int i = 0; i < massiv.Length; i++)
+    {
+        massiv[i] = number.Next(1, 200);
+        Console.Write($"{massiv[i]}  ");
+    }
+    Console.WriteLine();
+}
+
+void PrintArray()
+{
+    int count = 0;
+    foreach (int item in massiv)
+    {
+        if (item >= 10 && item <= 99)
+            count++;
+    }
+    Console.WriteLine($"Элементы из отрезка 10 - 99 встречаются {count} раз");
+}
+
+FillArray();
+PrintArray();
