dff6138d735d60622c0b690482045659270eebae
Odinokiy7 <koazo-maga@mail.ru>
2022-02-17 11:36:38 +0300

    Task 5

---
+++ task_5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+string? numberStr = Console.ReadLine();
+int a = int.Parse(numberStr);
+
+int y = 0;
+
+y = a * a;
+Console.WriteLine($"Если A = {a}, Y = {y}");
