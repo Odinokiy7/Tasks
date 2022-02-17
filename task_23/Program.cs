696f28a55086876877fc52a35533f3d85ea37286
Odinokiy7 <koazo-maga@mail.ru>
2022-02-17 13:57:56 +0300

    Task 23

---
+++ task_23/Program.cs
@@ -0,0 +1,13 @@
+﻿// 23. Показать таблицу квадратов чисел от 1 до N 
+
+Console.Clear();
+
+Console.WriteLine("Даны числа: ");
+int[] massiv = { 1, 2, 3, 4, 5, 6, 7, 8, };
+int count = massiv.Length;
+
+for (int i = 0; i < count; i++)
+{
+    Console.Write($"Число {massiv[i]} - ");
+    Console.WriteLine($"квадрат числа {massiv[i] * massiv[i]}");
+}
