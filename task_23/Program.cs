e49d0b73319fb27264d3d1985a8e879367c66a6d
Odinokiy7 <koazo-maga@mail.ru>
2022-02-22 16:29:50 +0300

    Task 23.2

--- task_23/Program.cs
+++ task_23/Program.cs
@@ -2,12 +2,21 @@
 
 Console.Clear();
 
-Console.WriteLine("Даны числа: ");
-int[] massiv = { 1, 2, 3, 4, 5, 6, 7, 8, };
-int count = massiv.Length;
+Console.WriteLine("Введите число от 1 до N: ");
+string? s1 = Console.ReadLine();
+int number1 = Convert.ToInt32(s1);
+string? s2 = Console.ReadLine();
+int number2 = Convert.ToInt32(s2);
+string? s3 = Console.ReadLine();
+int number3 = Convert.ToInt32(s3);
+string? s4 = Console.ReadLine();
+int number4 = Convert.ToInt32(s4);
+string? s5 = Console.ReadLine();
+int number5 = Convert.ToInt32(s5);
 
-for (int i = 0; i < count; i++)
-{
-    Console.Write($"Число {massiv[i]} - ");
-    Console.WriteLine($"квадрат числа {massiv[i] * massiv[i]}");
-}
+Console.WriteLine();
+Console.WriteLine($"{number1} * {number1} = {number1 * number1}");
+Console.WriteLine($"{number2} * {number2} = {number2 * number2}");
+Console.WriteLine($"{number3} * {number3} = {number3 * number3}");
+Console.WriteLine($"{number4} * {number4} = {number4 * number4}");
+Console.WriteLine($"{number5} * {number5} = {number5 * number5}");
