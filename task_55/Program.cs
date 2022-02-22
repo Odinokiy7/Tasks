cc88750b2db3c4901f9436fdcd472aa4e4df945a
Odinokiy7 <koazo-maga@mail.ru>
2022-02-22 16:26:17 +0300

    Task 55

---
+++ task_55/Program.cs
@@ -0,0 +1,47 @@
+﻿// // Задача 55. Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+
+Console.Clear();
+var arr = new int[3, 5];
+Random randomNumber = new Random();
+
+void FillArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = randomNumber.Next(1, 100);
+        }
+    }
+}
+
+void PrintArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]}   ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void AverageSum()
+{
+    double result = 0;
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            result = result + arr[i, j];
+        }
+        Console.WriteLine($"Средняя арифметическая {j+1} столбца = {result / arr.GetLength(0):F2}"); // :F2 - Помогает откинуть лишние цифры после запятой
+    }
+}
+
+Console.WriteLine("Даны столбцы: ");
+FillArray();
+PrintArray();
+Console.WriteLine("========================");
+AverageSum();
