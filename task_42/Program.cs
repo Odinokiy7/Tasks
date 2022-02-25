742dc924ba448f4ec4b5a0a3ce187a430a521576
Odinokiy7 <koazo-maga@mail.ru>
2022-02-25 10:18:53 +0300

    Task 42

---
+++ task_42/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 42: Определить сколько чисел больше 0 введено с клавиатуры
+
+Console.Clear();
+string? numberStr = String.Empty;
+int number = 1;
+Console.WriteLine("Введите число: ");
+for (int i = 0; number > 0; i++)
+{
+    numberStr = Console.ReadLine();
+    int.TryParse(numberStr, out number);
+    if (number < 1)
+    {
+        i--;
+        Console.WriteLine();
+        Console.WriteLine("Введенное число меньше 1, либо не число!");
+        break;
+    }
+    Console.WriteLine($"Количество введенных чисел = {i + 1}");
+    Console.Write("Введите следующее число: ");
+}
