8e8915e2684d8302f57bcf99c86ed626042621eb
Odinokiy7 <koazo-maga@mail.ru>
2022-02-25 15:14:33 +0300

    Task 48

---
+++ task_48/Program.cs
@@ -0,0 +1,29 @@
+﻿// 48. Показать двумерный массив размером m×n заполненный целыми числами.
+
+Console.Clear();
+int[,] massiv = new int[4, 10];
+Random number = new Random();
+Console.WriteLine("Дан двумерный массив: ");
+void FillArray()
+{
+    for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiv.GetLength(1); j++)
+        {
+            massiv[i, j] = number.Next(1, 10);
+        }
+    }
+}
+void PrintArray()
+{
+    for (int i = 0; i < massiv.GetLength(0); i++)
+    {
+        for (int j = 0; j < massiv.GetLength(1); j++)
+        {
+            Console.Write($"{massiv[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+FillArray();
+PrintArray();
