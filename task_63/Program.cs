c8bcc14859b9c00b1df39ddc3f94517440dcad3e
Odinokiy7 <koazo-maga@mail.ru>
2022-03-09 10:09:34 +0300

    Task 63

---
+++ task_63/Program.cs
@@ -0,0 +1,27 @@
+﻿// 63. Сформировать трехмерный массив не повторяющимися двузначными числами.
+//     Показать его построчно на экран, выводя индексы соответствующего элемента.
+
+Console.Clear();
+int[,,] array = new int[3, 3, 3];
+int mean = 20;
+
+void FillPrintArray()
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.WriteLine();
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = mean;
+                mean++;
+                Console.Write($"{array[i, j, k]} [{i}, {j}, {k}]   ");
+            }
+        }
+    }
+}
+
+Console.Write("Дан массив: ");
+FillPrintArray();
