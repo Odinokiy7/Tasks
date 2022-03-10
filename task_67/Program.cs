5f8d4d6967b31dc9f6e02d774e5c63a6a2bb1efa
Odinokiy7 <koazo-maga@mail.ru>
2022-03-10 09:56:49 +0300

    Task 67

---
+++ task_67/Program.cs
@@ -0,0 +1,13 @@
+﻿// 67. Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+Console.Write("Задайте значение для M = ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте значение для N = ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Все натуральные числа в промежутке от M до N:");
+while (m < n + 1)
+{
+    m++;
+    Console.Write($"{m - 1}   ");
+}
